cea0f0cc90d5b9bc4cb176570a6024a9a8200d37
uxsnap <nuxxx7132@gmail.com>
2024-02-08 20:31:35 +0300

    finish user page

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -42,7 +42,7 @@ module.exports = {
         test: /\.(css|sass|scss)$/,
         use: ['css-loader', 'sass-loader'],
       },
-      { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: 'asset/resource' },
+      { test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i, type: 'asset/resource' },
       { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: 'asset/inline' },
     ],
   },
