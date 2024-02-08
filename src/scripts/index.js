b7b85142d6e7ef0662c47207852afcaa0a484540
uxsnap <nuxxx7132@gmail.com>
2024-02-08 11:20:48 +0300

    finish side cart

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,10 @@
 const sideMenu = document.querySelector('.sideMenu')
 const header = document.querySelector('.header')
 const sideMenuBurger = sideMenu.querySelector('.sideMenuBurger')
-const headerBurger = header.querySelector('.menu')
+const headerBurger = header.querySelector('.menu');
+const sideCart = document.querySelector('.sideCart');
+const cartIcon = header.querySelector('.cart');
+const trashIcon = sideCart.querySelector('.trashIcon');
 
 headerBurger.onclick = function () {
   sideMenu.classList.add('active')
@@ -9,4 +12,12 @@ headerBurger.onclick = function () {
 
 sideMenuBurger.onclick = function () {
   sideMenu.classList.remove('active')
+}
+
+cartIcon.onclick = function() {
+  sideCart.classList.add('active');
+}
+
+trashIcon.onclick = function() {
+  sideCart.classList.remove('active');
 }
